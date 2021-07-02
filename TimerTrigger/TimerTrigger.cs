8890ef769087b4d76177ecea6c744a499e4e1fa0
Erik Nagel <erik@reallyhuman.net>
2021-07-02 10:24:54 +0200

    Auf TriggerBase umgestellt.

--- TimerTrigger/TimerTrigger.cs
+++ TimerTrigger/TimerTrigger.cs
@@ -2,56 +2,29 @@
 using System.Linq;
 using System.Reactive.Linq;
 using System.Threading;
-using System.Reflection;
 using Vishnu.Interchange;
 
 namespace TimerTrigger
 {
     /// <summary>
-    /// Löst in regelmäßigen, konfigurierbaren Zeitabständen das Event 'TriggerIt'
-    /// aus. Implementiert die Schnittstelle 'INodeTrigger' aus 'LogicalTaskTree.dll', über
+    /// Löst in regelmäßigen, konfigurierbaren Zeitabständen das beim Aufruf der "Start"-Routine
+    /// übergebene Event 'triggerIt' aus.
+    /// Implementiert die Schnittstelle 'INodeTrigger' aus 'Vishnu.Interchange.dll', über
     /// die sich der LogicalTaskTree von 'Vishnu' in das Event einhängen und den Trigger
     /// starten und stoppen kann.
     /// </summary>
     /// <remarks>
-    /// File: TimerTrigger.cs
     /// Autor: Erik Nagel
     ///
-    /// 19.07.2013 Erik Nagel: erstellt
+    /// 19.07.2013 Erik Nagel: erstellt.
     /// 15.12.2020 Erik Nagel: Warte-Durchlauf bei Übergabe von "UserRun" als letztem Parameter implementiert.
+    /// 26.06.2021 Erik Nagel: auf neue Basisklasse TriggerBase angepasst.
     /// </remarks>
-    public class TimerTrigger : INodeTrigger
+    public class TimerTrigger : TriggerBase
     {
         #region public members
 
         #region INodeTrigger Implementation
-
-        /// <summary>
-        /// Enthält Informationen zum besitzenden Trigger.
-        /// Implementiert sind NextRun und NextRunInfo. Für das Hinzufügen weiterer
-        /// Informationen kann diese Klasse abgeleitet werden.
-        /// </summary>
-        public TriggerInfo Info
-        {
-            get
-            {
-                string info = null;
-                if (this._triggerActive)
-                {
-                    info = this._nextTimerStart.ToString();
-                    this._info.NextRun = this._nextTimerStart;
-                }
-                else
-                {
-                    this._info.NextRun = DateTime.MinValue;
-                }
-                this._info.NextRunInfo = info;
-                return this._info;
-            }
-            set
-            {
-            }
-        }
 
         /// <summary>
         /// Startet den Trigger; vorher sollte sich der Consumer in TriggerIt eingehängt haben.
@@ -62,34 +35,99 @@ namespace TimerTrigger
         /// Einheiten sind: "MS" Millisekunden, "S" Sekunden, "M" Minuten, "H" Stunden und "D" Tage.</param>
         /// <param name="triggerIt">Die aufzurufende Callback-Routine, wenn der Trigger feuert.</param>
         /// <returns>True, wenn der Trigger durch diesen Aufruf tatsächlich gestartet wurde.</returns>
-        public bool Start(object triggerController, object triggerParameters, Action<TreeEvent> triggerIt)
+        public override bool Start(object triggerController, object triggerParameters, Action<TreeEvent> triggerIt)
         {
-            // TimeSpan firstRun, TimeSpan interval
-            bool isUserRun = triggerParameters.ToString().EndsWith("|UserRun");
+            base.Start(triggerController, triggerParameters, triggerIt);
 
+            // Trigger-spezifischer Code - Anfang
+            if (this._nodeCancellationTokenSource != null)
+            {
+                this._nodeCancellationTokenSource.Dispose();
+            }
+            this._nodeCancellationTokenSource = new CancellationTokenSource();
+            this._cancellationToken = this._nodeCancellationTokenSource.Token;
+            this._cancellationToken.Register(() => CancelNotification());
+            this._timerTask = Observable.Timer(this._firstRun, this._interval);
+            this._nextStart = DateTime.Now.AddMilliseconds(this._firstRun.TotalMilliseconds);
+            this._cancellationToken.Register(this._timerTask.Subscribe(this.OnTriggerFired).Dispose);
+            // Trigger-spezifischer Code - Ende
+
+            return true;
+        }
+
+        /// <summary>
+        /// Stoppt den Trigger.
+        /// </summary>
+        /// <param name="triggerController">Das Objekt, das den Trigger definiert.</param>
+        /// <param name="triggerIt">Die aufzurufende Callback-Routine, wenn der Trigger feuert.</param>
+        public override void Stop(object triggerController, Action<TreeEvent> triggerIt)
+        {
+            if (this._nodeCancellationTokenSource != null)
+            {
+                this._nodeCancellationTokenSource.Cancel();
+
+                // Weitergabe des Stop-Aufrufs an die Basisklasse
+                base.Stop(triggerController, triggerIt);
+            }
+        }
+
+        #endregion INodeTrigger Implementation
+
+        /// <summary>
+        /// Standard Konstruktor.
+        /// </summary>
+        public TimerTrigger() : base()
+        {
+            // Optional: Übergabe eines spezifischen Namens (ansonsten wird allgemein "Trigger" gesetzt)
+            this.TriggerName = "TimerTrigger";
+
+            // Optional: Formulierung einer Syntaxhilfe (wird im Fehlerfall ausgegeben)
+            this._syntaxInformation =
+                "Aufrufparameter: [Verzögerung] Intervall"
+              + Environment.NewLine
+              + "Format von Verzögerung, Interval: Einheit + ':' + ganzzahliger Wert"
+              + Environment.NewLine
+              + "Einheit: MS=Millisekunden, S=Sekunden, M=Minuten, H=Stunden, D=Tage."
+              + Environment.NewLine
+              + "Beispiel: TimerTrigger S:5 M:10 (feuert nach einer Wartezeit von 5 Sekunden alle 10 Minuten)";
+        }
+
+        #endregion public members
+
+        #region protected members
+
+        /// <summary>
+        /// Diese Routine wird von der Routine "Start" angesprungen, bevor der Trigger gestartet wird.
+        /// Erweitert TriggerBase.EvaluateParametersOrFail; dort wird nur der Parameter "|UserRun"
+        /// ausgewertet und die Variable "_isUserRun" entsprechend gesetzt.
+        /// </summary>
+        /// <param name="triggerParameters">Die von Vishnu weitergeleiteten Parameter aus der JobDescription.xml.</param>
+        /// <param name="triggerController">Der Knoten, dem dieser Trigger zugeordnet ist.</param>
+        protected override void EvaluateParametersOrFail(ref object triggerParameters, object triggerController)
+        {
+            base.EvaluateParametersOrFail(ref triggerParameters, triggerController);
+
+            // Ab hier folgt die Trigger-spezifische Prüfung und Übernahme der von Ihnen über die JobDescription.xml
+            // übergebenen Parameter. In %-Zeichen eingeschlossene Zeichenketten wurden ggf. von der
+            // Vishnu-Parameterersetzung durch ihre aktuellen Laufzeitwerte ersetzt.
             string firstArg = (triggerParameters.ToString() + "|").Split('|')[0];
             string secondArg = "";
             if (!firstArg.Equals(""))
             {
                 secondArg = (triggerParameters.ToString() + "|").Split('|')[1];
-                if (secondArg.Equals("") || secondArg.Equals("UserRun"))
-                {
-                    secondArg = firstArg;
-                    firstArg = "S:0"; // 0 Sekunden bis zum ersten Trigger-Event
-                }
             }
             else
             {
-                this.syntax("Es muss zumindest eine Zeitangabe erfolgen.");
+                this.ThrowSyntaxException("Es muss zumindest eine Zeitangabe erfolgen.");
             }
-            if (isUserRun)
+            if (this._isUserRun)
             {
-                firstArg = secondArg; // erstmal einen Duschlauf warten, da der Knoten durch UserRun schon direkt gestartet wurde.
+                firstArg = secondArg; // erstmal einen Durchlauf warten, da der Knoten durch UserRun schon direkt gestartet wurde.
             }
             string firstUnity = (firstArg + ":").Split(':')[0].ToUpper();
             if (!(new string[] { "MS", "S", "M", "H", "D" }).Contains(firstUnity))
             {
-                this.syntax("Es wurde keine gültige Einheit (MS, S, M, H, D) angegeben.");
+                this.ThrowSyntaxException("Es wurde keine gültige Einheit (MS, S, M, H, D) angegeben.");
             }
             double firstValue = Convert.ToDouble((firstArg + ":").Split(':')[1]); // wirft eventuell eine Exception, muss dann so sein
             switch (firstUnity)
@@ -103,7 +141,7 @@ namespace TimerTrigger
             string secondUnity = (secondArg + ":").Split(':')[0].ToUpper();
             if (!(new string[] { "MS", "S", "M", "H", "D" }).Contains(secondUnity))
             {
-                this.syntax("Es wurde keine gültige Einheit (MS, S, M, H, D) angegeben.");
+                this.ThrowSyntaxException("Es wurde keine gültige Einheit (MS, S, M, H, D) angegeben.");
             }
             double secondValue = Convert.ToDouble((secondArg + ":").Split(':')[1]); // wirft eventuell eine Exception, muss dann so sein
             switch (secondUnity)
@@ -114,85 +152,19 @@ namespace TimerTrigger
                 case "H": this._interval = TimeSpan.FromHours(Convert.ToDouble(secondValue)); break;
                 case "D": this._interval = TimeSpan.FromDays(Convert.ToDouble(secondValue)); break;
             }
-            if (this.NodeCancellationTokenSource != null)
-            {
-                this.NodeCancellationTokenSource.Dispose();
-            }
-            this.triggerIt += triggerIt;
-            this.NodeCancellationTokenSource = new CancellationTokenSource();
-            this._cancellationToken = this.NodeCancellationTokenSource.Token;
-            this._cancellationToken.Register(() => cancelNotification());
-            this._timerTask = Observable.Timer(this._firstRun, this._interval);
-            this._lastTimerStart = DateTime.MinValue;
-            this._nextTimerStart = DateTime.Now.AddMilliseconds(this._firstRun.TotalMilliseconds);
-            this._cancellationToken.Register(this._timerTask.Subscribe(this.onTriggerFired).Dispose);
-            this._triggerActive = true;
-            return true;
-        }
-
-        private void syntax(string errorMessage)
-        {
-            string exeName = Assembly.GetExecutingAssembly().GetName().Name;
-            throw new ArgumentException(exeName
-              + ": " + errorMessage
-              + Environment.NewLine
-              + "Aufruf: " + exeName + " [Verzögerung] Interval"
-              + Environment.NewLine
-              + "Format von Verzögerung, Interval: Einheit + ':' + ganzzahliger Wert"
-              + Environment.NewLine
-              + "Einheit: MS=Millisekunden, S=Sekunden, M=Minuten, H=Stunden, D=Tage."
-              + Environment.NewLine
-              + "Beispiel: TimerTrigger S:5 M:10 (feuert nach einer Wartezeit von 5 Sekunden alle 10 Minuten)"
-             );
         }
 
         /// <summary>
-        /// Stoppt den Trigger.
-        /// </summary>
-        /// <param name="triggerController">Das Objekt, das den Trigger aufruft.</param>
-        /// <param name="triggerIt">Die aufzurufende Callback-Routine, wenn der Trigger feuert.</param>
-        public void Stop(object triggerController, Action<TreeEvent> triggerIt)
-        {
-            if (this.NodeCancellationTokenSource != null)
-            {
-                this.NodeCancellationTokenSource.Cancel();
-                this._triggerActive = false;
-                this._lastTimerStart = DateTime.Now;
-                this._nextTimerStart = this._lastTimerStart.AddMilliseconds(this._interval.TotalMilliseconds);
-                this.triggerIt -= triggerIt;
-            }
-        }
-
-        #endregion INodeTrigger Implementation
-
-        /// <summary>
-        /// Standard Konstruktor.
-        /// </summary>
-        public TimerTrigger()
-        {
-            this._info = new TriggerInfo() { NextRun = DateTime.MinValue, NextRunInfo = null };
-            this._lastTimerStart = DateTime.MinValue;
-            this._nextTimerStart = DateTime.MinValue;
-            this._triggerActive = false;
-        }
-
-        #endregion public members
-
-        #region protected members
-
-        /// <summary>
-        /// Trigger-Event auslösen.
+        /// Diese Routine löst das Trigger-Event aus.
+        /// Für ein Setzen der Variablen "_lastStart" und "_nextStart" überschreibt diese Routine
+        /// TriggerBase.OnTriggerFired.
         /// </summary>
         /// <param name="dummy">Aus Kompatibilitätsgründen, wird hier nicht genutzt.</param>
-        protected void onTriggerFired(long dummy)
+        protected override void OnTriggerFired(long dummy)
         {
-            this._lastTimerStart = DateTime.Now;
-            this._nextTimerStart = this._lastTimerStart.AddMilliseconds(this._interval.TotalMilliseconds);
-            if (this.triggerIt != null)
-            {
-                this.triggerIt(new TreeEvent("TimerTrigger", "TimerTrigger", this.Info.NextRunInfo,
-                                  "", "", null, 0, null, null));
-            }
+            this._lastStart = DateTime.Now;
+            this._nextStart = this._lastStart.AddMilliseconds(this._interval.TotalMilliseconds);
+            base.OnTriggerFired(dummy);
         }
 
         #endregion protected members
@@ -200,17 +172,8 @@ namespace TimerTrigger
         #region private members
 
         private IObservable<long> _timerTask;
-        private CancellationTokenSource NodeCancellationTokenSource { get; set; }
+        private CancellationTokenSource _nodeCancellationTokenSource { get; set; }
         private CancellationToken _cancellationToken;
-        private TriggerInfo _info;
-        private DateTime _lastTimerStart;
-        private DateTime _nextTimerStart;
-        private bool _triggerActive;
-
-        /// <summary>
-        /// Wird ausgelöst, wenn das Trigger-Ereignis (z.B. Timer) eintritt. 
-        /// </summary>
-        private event Action<TreeEvent> triggerIt;
 
         /// <summary>
         /// Definierter Zeitpunkt für den ersten Start
@@ -220,14 +183,12 @@ namespace TimerTrigger
 
         /// <summary>
         /// Intervall für den wiederholten Start (Run)
-        /// der zugeorneten INodeWorkr oder null.
+        /// der zugeorneten INodeWorker oder null.
         /// </summary>
         private TimeSpan _interval { get; set; }
 
-        /// <summary>
-        /// Informiert über den Abbruch der Verarbeitung.
-        /// </summary>
-        private void cancelNotification()
+        // Informiert über den Abbruch der Verarbeitung.
+        private void CancelNotification()
         {
             // noppes
         }
--- TimerTriggerDemo/Program.cs
+++ TimerTriggerDemo/Program.cs
@@ -10,16 +10,17 @@ namespace TimerTriggerDemo
         {
             TimerTrigger.TimerTrigger trigger = new TimerTrigger.TimerTrigger();
             trigger.Start(null, @"S:5|S:3", trigger_TriggerIt);
-            Console.WriteLine("stop trigger mit enter");
+            Console.WriteLine("Stopp Trigger mit Enter");
+            Console.WriteLine();
             Console.ReadLine();
             trigger.Stop(null, trigger_TriggerIt);
-            Console.WriteLine("Trigger stopped");
+            Console.WriteLine("Trigger gestoppt");
             Console.ReadLine();
         }
 
         static void trigger_TriggerIt(TreeEvent source)
         {
-            Console.WriteLine(String.Format($"Trigger feuert ({source.SourceId}{source.SenderId})."));
+            Console.WriteLine($"{source.Name} feuert ({source.SourceId} {source.SenderId}).");
             try
             {
                 File.SetLastWriteTimeUtc(@"c:\Logs\Vishnu\CheckAll\JobSnapshot.xml", DateTime.UtcNow);
